4a564e930f7a1de11c882d2e0f88d536159a25f3
MHanson <michaelhanson11@augustana.edu>
2015-04-17 15:05:19 -0500

    Added JoinSessionActivity.java

---
+++ app/src/main/java/com/example/emilyleazer11/votingapp/JoinSessionActivity.java
@@ -0,0 +1,15 @@
+package com.example.emilyleazer11.votingapp;
+
+import android.app.Activity;
+import android.os.Bundle;
+/**
+ * Created by MHanson on 4/17/2015.
+ */
+public class JoinSessionActivity extends Activity {
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_join_session);
+    }
+}
+
+
