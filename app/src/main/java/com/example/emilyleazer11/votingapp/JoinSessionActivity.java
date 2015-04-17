0bd4e3d433e1530101869c74dd029eef54327f28
emilyleazer11 <emilyleazer11@augustana.edu>
2015-04-17 15:21:16 -0500

    Trying to push JoinSessionActivity up to gitHub

--- app/src/main/java/com/example/emilyleazer11/votingapp/JoinSessionActivity.java
+++ app/src/main/java/com/example/emilyleazer11/votingapp/JoinSessionActivity.java
@@ -2,13 +2,24 @@ package com.example.emilyleazer11.votingapp;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
+
 /**
  * Created by MHanson on 4/17/2015.
  */
 public class JoinSessionActivity extends Activity {
+
+
+    public static final String TAG = "Voting App Activity";
+
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_join_session);
+
+        //doing nothing
+        Log.w(TAG, "test");
+
+
     }
 }
 
