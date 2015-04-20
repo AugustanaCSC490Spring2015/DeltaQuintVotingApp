2c1115e919fc1393cde8eeec774a571f286f1820
MHanson <michaelhanson11@augustana.edu>
2015-04-20 18:08:51 -0500

    Added functionality to Join Existing Screen button and created SessionActivity java class and activity_session.xml

--- app/src/main/java/com/example/emilyleazer11/votingapp/JoinSessionActivity.java
+++ app/src/main/java/com/example/emilyleazer11/votingapp/JoinSessionActivity.java
@@ -1,8 +1,11 @@
 package com.example.emilyleazer11.votingapp;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 /**
  * Created by MHanson on 4/17/2015.
@@ -12,15 +15,29 @@ public class JoinSessionActivity extends Activity {
 
     public static final String TAG = "Voting App Activity";
 
-    protected void onCreate(Bundle savedInstanceState){
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_join_session);
 
-        //doing nothing
-        Log.w(TAG, "test");
+        Button joinSession = (Button) findViewById(R.id.joinSessionButton);
+        joinSession.setOnClickListener(createClickListener);
+
+    }
+
+    View.OnClickListener createClickListener = new View.OnClickListener() {
+        @Override
+            public void onClick(View v){
+                joinSession();
+            }
+        };
+
+    public void joinSession(){
+         Intent intent = new Intent (this, SessionActivity.class);
+         startActivity(intent);
+    }
 
 
     }
-}
+
 
 
---
+++ app/src/main/java/com/example/emilyleazer11/votingapp/SessionActivity.java
@@ -0,0 +1,15 @@
+package com.example.emilyleazer11.votingapp;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by MHanson on 4/20/2015.
+ */
+public class SessionActivity extends Activity {
+    protected void onCreate(Bundle savedInstanceState){
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_session);
+    }
+}
