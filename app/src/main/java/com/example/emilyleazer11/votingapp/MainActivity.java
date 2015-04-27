1b25c0e6dbd2b0f41e9ce03cecc2563c57002ce3
erikaksland11 <erikaksland11@augustana.edu>
2015-04-26 19:50:44 -0500

    check for FINISHING on the activity_create_session.xml check for FINISHING on the activity_join_session.xml
    editted some code so the onClick is Used instead of creating listeners and having unnecessary code

--- app/src/main/java/com/example/emilyleazer11/votingapp/MainActivity.java
+++ app/src/main/java/com/example/emilyleazer11/votingapp/MainActivity.java
@@ -6,10 +6,9 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.Button;
 
-import com.parse.Parse;
-import com.parse.ParseObject;
+//import com.parse.Parse;
+//import com.parse.ParseObject;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -18,38 +17,18 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        Button createSession = (Button) findViewById(R.id.createButton);
-        createSession.setOnClickListener(createClickListener);
-
-        Button joinSession = (Button) findViewById(R.id.joinButton);
-        joinSession.setOnClickListener(joinClickListener);
-
     }
 
-    View.OnClickListener createClickListener = new View.OnClickListener() {
-        @Override
-        public void onClick(View v){
-            createNewSession();
-        }
-    };
-
-    View.OnClickListener joinClickListener = new View.OnClickListener() {
-        @Override
-        public void onClick(View v){
-            joinExistingSession();
-        }
-    };
-
-    public void createNewSession(){
+    public void createNewSession(View view){
         Intent intent = new Intent(this, CreateSessionActivity.class);
         startActivity(intent);
     }
 
-    public void joinExistingSession(){
+    public void joinExistingSession(View view){
         Intent intent = new Intent (this, JoinSessionActivity.class);
         startActivity(intent);
     }
+
 
 
     @Override
