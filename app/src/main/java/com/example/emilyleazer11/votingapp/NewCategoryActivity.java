e181cb19a641f78ed86abc93aede8742d24b5c32
MHanson <michaelhanson11@augustana.edu>
2015-04-29 15:52:04 -0500

    Finished activity_new_category list

--- app/src/main/java/com/example/emilyleazer11/votingapp/NewCategoryActivity.java
+++ app/src/main/java/com/example/emilyleazer11/votingapp/NewCategoryActivity.java
@@ -1,31 +1,70 @@
 package com.example.emilyleazer11.votingapp;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.app.AlertDialog;
+import android.app.ListActivity;
+import android.content.DialogInterface;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.AdapterView.OnItemLongClickListener;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+
+
 
 /**
  * Created by emilyleazer11 on 4/22/2015.
  */
-public class NewCategoryActivity extends Activity{
+public class NewCategoryActivity extends ListActivity {
 
-    protected void onCreate(Bundle savedInstanceState) {
+    private static final String CANDIDATES = "candidates";
+    private EditText candidateEditText;
+    private SharedPreferences savedCandidates;
+    private ArrayList<String> candidates;
+    private ArrayAdapter<String> adapter;
+@Override
+protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_category);
 
         Button endVotingSessionButton = (Button) findViewById(R.id.endVotingSessionButton);
         endVotingSessionButton.setOnClickListener(endSessionClickListener);
 
+        candidateEditText = (EditText) findViewById(R.id.candidateEditText);
+        savedCandidates = getSharedPreferences(CANDIDATES, MODE_PRIVATE);
+
+        // store the saved candidates in an ArrayList then sort them
+        candidates = new ArrayList<String>(savedCandidates.getAll().keySet());
+        Collections.sort(candidates, String.CASE_INSENSITIVE_ORDER);
+
+        adapter = new ArrayAdapter<String>(this,R.layout.list_item, candidates);
+        setListAdapter(adapter);
+
+        Button addCandidateButton = (Button) findViewById(R.id.addCandidateButton);
+        addCandidateButton.setOnClickListener(addCandidateButtonListener);
+
+        getListView().setOnItemLongClickListener(candidateLongClickListener);
+
         Button viewResultsButton = (Button) findViewById(R.id.viewResultsButton);
         viewResultsButton.setOnClickListener(viewResultsClickListener);
 
     }
 
-    View.OnClickListener endSessionClickListener = new View.OnClickListener(){
+    View.OnClickListener endSessionClickListener = new View.OnClickListener() {
         @Override
-        public void onClick(View v){
+        public void onClick(View v) {
             launchMainActivity();
         }
     };
@@ -37,8 +76,8 @@ public class NewCategoryActivity extends Activity{
         }
     };
 
-
-    public void launchMainActivity(){
+    public void launchMainActivity() {
+        savedCandidates.edit().clear().commit();
         Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
     }
@@ -47,4 +86,165 @@ public class NewCategoryActivity extends Activity{
         Intent intent = new Intent(this, ResultsActivity.class);
         startActivity(intent);
     }
+
+
+    public View.OnClickListener addCandidateButtonListener = new View.OnClickListener(){
+        @Override
+        public void onClick(View v){
+            if(candidateEditText.getText().length()>0){
+                addCandidate(candidateEditText.getText().toString());
+                candidateEditText.setText("");
+            }else{
+                // create a new AlertDialog Builder
+                AlertDialog.Builder builder =
+                        new AlertDialog.Builder(NewCategoryActivity.this);
+
+                // set dialog's message to display
+                builder.setMessage("No candidate entered.");
+
+                // provide an OK button that simply dismisses the dialog
+                builder.setPositiveButton("Ok", null);
+
+                // create AlertDialog from the AlertDialog.Builder
+                AlertDialog errorDialog = builder.create();
+                errorDialog.show(); // display the modal dialog
+            }
+        }
+    };
+
+    private void addCandidate(String candidateName){
+        SharedPreferences.Editor preferencesEditor = savedCandidates.edit();
+        preferencesEditor.putString(candidateName, candidateName);
+        preferencesEditor.apply();
+
+        if(!candidates.contains(candidateName)){
+            candidates.add(candidateName);
+            Collections.sort(candidates, String.CASE_INSENSITIVE_ORDER);
+            adapter.notifyDataSetChanged();
+        }
+    }
+
+    OnItemLongClickListener candidateLongClickListener = new OnItemLongClickListener(){
+        @Override
+        public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id){
+            final String savedCandidate = ((TextView) view).getText().toString();
+
+            AlertDialog.Builder builder = new AlertDialog.Builder(NewCategoryActivity.this);
+
+            builder.setTitle(getString(R.string.shareEditDeleteTitle, savedCandidate));
+
+            builder.setItems(R.array.dialog_items,
+                    new DialogInterface.OnClickListener(){
+                        @Override
+                        public void onClick(DialogInterface dialog, int which){
+                            switch (which){
+                                case 0: //edit
+                                    candidateEditText.setText(savedCandidates.getString(savedCandidate, ""));
+                                    editCandidate(savedCandidate);
+                                    break;
+                                case 1: //delete
+                                    deleteCandidate(savedCandidate);
+                                    break;
+
+
+                            }
+                        }
+
+                    }
+            );
+
+            builder.setNegativeButton(getString(R.string.cancel),
+                    new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int id){
+                            dialog.cancel();
+                        }
+                    }
+            );
+
+            builder.create().show();
+            return true;
+
+        }
+    };
+
+    private void editCandidate(final String tag){
+        AlertDialog.Builder confirmBuilder = new AlertDialog.Builder(this);
+        confirmBuilder.setMessage("Are you sure you want to edit this candidate?");
+
+        confirmBuilder.setNegativeButton(getString(R.string.cancel),
+                new DialogInterface.OnClickListener(){
+                    public void onClick(DialogInterface dialog, int id){
+                        dialog.cancel();
+                    }
+
+                }
+        );
+
+        confirmBuilder.setPositiveButton(getString(R.string.OK),
+                new DialogInterface.OnClickListener()
+                {
+                    // called when "Cancel" Button is clicked
+                    public void onClick(DialogInterface dialog, int id)
+                    {
+                        candidates.remove(tag); // remove tag from quantities
+
+                        // get SharedPreferences.Editor to remove saved search
+                        SharedPreferences.Editor preferencesEditor =
+                                savedCandidates.edit();
+                        preferencesEditor.remove(tag); // remove search
+                        preferencesEditor.apply(); // saves the changes
+
+                        // rebind quantities ArrayList to ListView to show updated list
+                        adapter.notifyDataSetChanged();
+                    }
+                } // end OnClickListener
+        ); // end call to setPositiveButton
+
+        confirmBuilder.create().show();
+    }
+
+    private void deleteCandidate(final String tag)
+    {
+        // create a new AlertDialog
+        AlertDialog.Builder confirmBuilder = new AlertDialog.Builder(this);
+
+        // set the AlertDialog's message
+        confirmBuilder.setMessage(
+                getString(R.string.confirmMessage, tag));
+
+        // set the AlertDialog's negative Button
+        confirmBuilder.setNegativeButton( getString(R.string.cancel),
+                new DialogInterface.OnClickListener()
+                {
+                    // called when "Cancel" Button is clicked
+                    public void onClick(DialogInterface dialog, int id)
+                    {
+                        dialog.cancel(); // dismiss dialog
+                    }
+                }
+        ); // end call to setNegativeButton
+
+        // set the AlertDialog's positive Button
+        confirmBuilder.setPositiveButton(getString(R.string.delete),
+                new DialogInterface.OnClickListener()
+                {
+                    // called when "Cancel" Button is clicked
+                    public void onClick(DialogInterface dialog, int id)
+                    {
+                        candidates.remove(tag); // remove tag from quantities
+
+                        // get SharedPreferences.Editor to remove saved search
+                        SharedPreferences.Editor preferencesEditor =
+                                savedCandidates.edit();
+                        preferencesEditor.remove(tag); // remove search
+                        preferencesEditor.apply(); // saves the changes
+
+                        // rebind quantities ArrayList to ListView to show updated list
+                        adapter.notifyDataSetChanged();
+                    }
+                } // end OnClickListener
+        ); // end call to setPositiveButton
+
+        confirmBuilder.create().show(); // display AlertDialog
+    } // end method deleteSearch
 }
