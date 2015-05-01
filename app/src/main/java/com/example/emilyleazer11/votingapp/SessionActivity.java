45213f1e49ffa90a1e95d91bc9fc1ae17fd91968
Reed <ReedKottke11@augustana.edu>
2015-05-01 11:35:51 -0700

    in the SessionActivity java page, the method at the bottom is ready to populate the page so that when a user logs into a session, they can see the possible candidates to vote for.

--- app/src/main/java/com/example/emilyleazer11/votingapp/SessionActivity.java
+++ app/src/main/java/com/example/emilyleazer11/votingapp/SessionActivity.java
@@ -3,13 +3,25 @@ package com.example.emilyleazer11.votingapp;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+
+import com.parse.FindCallback;
+import com.parse.ParseException;
+import com.parse.ParseQuery;
+
+import java.util.List;
 
 /**
  * Created by MHanson on 4/20/2015.
  */
 public class SessionActivity extends Activity {
+    //there will need to be some variable, maybe passed by the intent, or by the sharedPrefManager
+    // that will define what sesion this user is logged into and what category is up
+    //right now i have itset to "NewSession"
+    public final String sessionName = "NewSession";
+    public final String currentCategory = "Treasurer";
     protected void onCreate(Bundle savedInstanceState){
 
         super.onCreate(savedInstanceState);
@@ -29,6 +41,37 @@ public class SessionActivity extends Activity {
     public void submitVote(){
         Intent intent = new Intent(this, EndSessionActivity.class);
         startActivity(intent);
+    }
+
+    //query: pul all candidates of one session and one category
+    public void populateCandidates() {
+        ParseQuery<Candidate> query = ParseQuery.getQuery("Candidates");
+        query.whereEqualTo("session_name", sessionName);
+        query.whereEqualTo("position", currentCategory);
+        query.findInBackground(new FindCallback<Candidate>() {
+            public void done(List<Candidate> candidateList, ParseException e) {
+                if (e == null) {
+                    fillList(candidateList, currentCategory);
+                    //no error
+                } else {
+                    Log.w("session_name", "Error: " + e.getMessage());
+                }
+            }
+        });
+    }
+
+    public void fillList(List<Candidate> candidateList, String category) {
+
+     //this method should populate the page with all possible candidates.
+     //the List object candidateList full of object types "Candidate"
+     //will contain all of the candidates in the candidate table
+     //that have a session_name of "NewSession" and
+     //position of "Treasurer"
+      final String categoryName = category;
+      for (Candidate candidate: candidateList) {
+          candidate.getCandidateName();
+          //this will loop through all of the candidate objects and return all of the names
+      }
     }
 
 
