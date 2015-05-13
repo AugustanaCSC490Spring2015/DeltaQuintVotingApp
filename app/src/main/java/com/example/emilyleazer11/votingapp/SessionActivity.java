7dc56512d8cb91f0190dd0f22b18546fa2bdc383
erikaksland11 <erikaksland11@augustana.edu>
2015-05-12 22:49:32 -0500

    started to be able to show names in the layout from the java code needs finishing, text boxes made but no text is shown

--- app/src/main/java/com/example/emilyleazer11/votingapp/SessionActivity.java
+++ app/src/main/java/com/example/emilyleazer11/votingapp/SessionActivity.java
@@ -6,20 +6,25 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
 import android.widget.TextView;
 
 import com.parse.FindCallback;
 import com.parse.ParseException;
 import com.parse.ParseQuery;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 
 public class SessionActivity extends Activity {
+    List<Map<String, String>> candidateMappedList = new ArrayList<Map<String,String>>();
     //there will need to be some variable, maybe passed by the intent, or by the sharedPrefManager
     // that will define what sesion this user is logged into and what category is up
     //right now i have itset to "NewSession"
-
 
     protected void onCreate(Bundle savedInstanceState){
 
@@ -33,7 +38,17 @@ public class SessionActivity extends Activity {
         session.setText(sessionIntent);
 
         //populate list of possible candidates to vote for here (in the category: categoryIntent)
-        populateCandidates();
+        //populateCandidates();
+
+        // beginTest
+        generateList();
+        ListView lv = (ListView) findViewById(R.id.candidatesListView);
+        // this is the adapter that will help show the list
+        SimpleAdapter ourSimpleAdapter = new SimpleAdapter(this, candidateMappedList,
+                android.R.layout.simple_list_item_1, new String[]{"category"},
+                new int[]{android.R.id.text1});
+        lv.setAdapter(ourSimpleAdapter);
+        // endTest
 
         Button submitButton = (Button) findViewById(R.id.submitButton);
         submitButton.setOnClickListener(submitClickListener);
@@ -51,7 +66,7 @@ public class SessionActivity extends Activity {
         startActivity(intent);
     }
 
-    //query: pul all candidates of one session and one category
+    //query: pull all candidates of one session and one category
     public void populateCandidates() {
         Intent starterIntent = this.getIntent();
         String sessionIntent = starterIntent.getStringExtra(JoinSessionActivity.SESSION_EXTRA);
@@ -73,18 +88,42 @@ public class SessionActivity extends Activity {
 
     public void fillList(List<Candidate> candidateList, String category) {
 
-     //this method should populate the page with all possible candidates.
-     //the List object candidateList full of object types "Candidate"
-     //will contain all of the candidates in the candidate table
-     //that have a session_name of "NewSession" and
-     //position of "Treasurer"
-      final String categoryName = category;
-      for (Candidate candidate: candidateList) {
-          candidate.getCandidateName();
-          //this will loop through all of the candidate objects and return all of the names
-      }
+        //this method should populate the page with all possible candidates.
+        //the List object candidateList full of object types "Candidate"
+        //will contain all of the candidates in the candidate table
+        //that have a session_name of "NewSession" and
+        //position of "Treasurer"
+        List<Map<String, String>> candidateMappedList = new ArrayList<Map<String,String>>();
+        final String categoryName = category;
+        for (Candidate candidate: candidateList) {
+            String currentCandidate = candidate.getCandidateName();
+            candidateMappedList.add(createCandidate(categoryName, currentCandidate));
+            //generateList();
+        }
+        displayList(candidateMappedList);
     }
 
+    public void displayList(List<Map<String, String>> candidateMappedList) {
+        ListView lv = (ListView) findViewById(R.id.candidatesListView);
+
+        // this is the adapter that will help show the list
+        SimpleAdapter ourSimpleAdapter = new SimpleAdapter(this, candidateMappedList,
+                android.R.layout.simple_list_item_1, new String[]{"category"},
+                new int[]{android.R.id.text1});
+        lv.setAdapter(ourSimpleAdapter);
+    }
+
+    private HashMap<String, String> createCandidate(String key, String name) {
+        HashMap<String, String> tempCandidate = new HashMap<String, String>();
+        tempCandidate.put(key, name);
+        return tempCandidate;
+    }
+
+    private void generateList() {
+        candidateMappedList.add(createCandidate("test", "Erik"));
+        candidateMappedList.add(createCandidate("test", "Chris"));
+        candidateMappedList.add(createCandidate("test", "Alex"));
+    }
 
 
 
